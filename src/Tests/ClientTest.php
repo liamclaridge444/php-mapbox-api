887e9f024a03db6d14f8c95b17b0a7b2e72c0c04
Liam Claridge <liam@oceanfarmr.com>
2023-11-11 16:54:45 +0700

    Add a test suite.

---
+++ src/Tests/ClientTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Mapbox\Tests;
+
+use BadMethodCallException;
+use InvalidArgumentException;
+use MapboxApi\Api;
+use MapboxApi\Client;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientInterface;
+
+class ClientTest extends TestCase
+{
+    /**
+     * @test ->getHttpClient()
+     */
+    public function testCreateWithoutHttpClient()
+    {
+        $client = new Client('test-token');
+
+        $this->assertInstanceOf(ClientInterface::class, $client->getHttpClient());
+    }
+
+    /**
+     * @test ->getHttpClient()
+     */
+    public function testCreateWithHttpClient()
+    {
+        $mockClientInterface = $this->getMockBuilder(ClientInterface::class)
+            ->getMock();
+
+        $client = Client::createWithHttpClient('test-token', $mockClientInterface);
+
+        $this->assertInstanceOf(ClientInterface::class, $client->getHttpClient());
+    }
+
+    /**
+     * @test ->__call()
+     */
+    public function testCallMagicMethodException()
+    {
+        $this->expectException(BadMethodCallException::class);
+        $this->expectExceptionMessage('Undefined API instance: "doesNotExist"');
+        
+        $client = new Client('test-token');
+        $client->doesNotExist();
+    }
+
+    /**
+     * @test ->__call()
+     */
+    public function testCallMagicMethod()
+    {
+        $client = new Client('test-token');
+        $api = $client->datasets();
+
+        $this->assertInstanceOf(\MapboxApi\Api\Datasets::class, $api);
+    }
+
+    /**
+     * @test ->api()
+     */
+    public function testApiDoesNotExist()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Undefined API instance: "doesNotExist"');
+
+        $client = new Client('test-token');
+        $client->api('doesNotExist');
+    }
+
+    /**
+     * @test ->api()
+     *
+     * @dataProvider getApiClassesProvider
+     */
+    public function testApiInstance($apiName, $class)
+    {
+        $client = new Client('test-token');
+
+        $this->assertInstanceOf($class, $client->api($apiName));
+    }
+
+    public function getApiClassesProvider()
+    {
+        return [
+            ['datasets', Api\Datasets::class],
+        ];
+    }
+}
