555f70b088b1111ed0d3a29dbffd7311398a2543
Liam Claridge <liam@oceanfarmr.com>
2024-01-30 16:24:43 +0700

    Add tests.

--- src/Tests/Api/DatasetsTest.php
+++ src/Tests/Api/DatasetsTest.php
@@ -218,6 +218,59 @@ class DatasetsTest extends TestCase
     }
 
     /**
+     * @test ->update()
+     */
+    public function testUpdate()
+    {
+        $mockStreamInterface = $this->createMock(StreamInterface::class);
+        $mockStreamInterface->expects($this->once())
+            ->method('__toString')
+            ->willReturn($this->mockUpdateResponseData());
+
+        $mockResponse = $this->createMock(ResponseInterface::class);
+        $mockResponse->expects($this->once())
+            ->method('getStatusCode')
+            ->willReturn(200);
+        $mockResponse->expects($this->once())
+            ->method('getHeaders')
+            ->willReturn(['Content-Type' => ['application/json']]);
+        $mockResponse->expects($this->once())
+            ->method('getBody')
+            ->willReturn($mockStreamInterface);
+
+        $response = new Response($mockResponse);
+
+        $username = 'scothis';
+        $datasetId = '1234';
+        $endpoint = sprintf('/%s/%s/%s/%s', Datasets::API_NAME, Datasets::API_VERSION, $username, $datasetId);
+        $data = ['name' => 'baz', 'description' => 'foo-bar'];
+        $accessToken = 'pk.eyJ1Ijoic2NvdGhpcyIsImEiOiJjaWp1Y2ltYmUwMDBicmJrdDQ4ZDBkaGN4In0.sbihZCZJ56-fsFNKHXF8YQ';
+
+        $mockClient = $this->getMockBuilder(Client::class)
+            ->setConstructorArgs([$accessToken])
+            ->getMock();
+        $mockClient->expects($this->once())
+            ->method('getAccessToken')
+            ->willReturn($accessToken);
+
+        $mockDatasets = $this->getMockBuilder(Datasets::class)
+            ->setConstructorArgs([$mockClient])
+            ->onlyMethods(['patch'])
+            ->getMock();
+
+        $mockDatasets->expects($this->once())
+            ->method('patch')
+            ->with($endpoint, $data)
+            ->willReturn($response);
+
+        $apiResponse = $mockDatasets->update('1234', $data);
+
+        $this->assertSame(200, $apiResponse->getStatusCode());
+        $this->assertSame(['Content-Type' => ['application/json']], $apiResponse->getHeaders());
+        $this->assertSame($this->mockUpdateResponseData(), $apiResponse->getContent());
+    }
+
+    /**
      * @link: https://docs.mapbox.com/api/maps/datasets/#example-response-list-datasets
      */
     private function mockListResponseData(): string
@@ -281,6 +334,24 @@ class DatasetsTest extends TestCase
             "size": 409600,
             "name": "foo",
             "description": "bar"
+          }';
+    }
+
+    /**
+     * @link: https://docs.mapbox.com/api/maps/datasets/#example-response-update-a-dataset
+     */
+    private function mockUpdateResponseData(): string
+    {
+        return '{
+            "owner": "scothis",
+            "id": "1234",
+            "created": "2023-12-18T14:24:33.296Z",
+            "modified": "2023-12-18T14:24:33.296Z",
+            "bounds": [-10, -10, 10, 10],
+            "features": 100,
+            "size": 409600,
+            "name": "baz",
+            "description": "foo-bar"
           }';
     }
 }
